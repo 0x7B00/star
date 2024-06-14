1690a99865b7df97afd9f0edea21230deb6031e3
smile349 <122448836+smile349@users.noreply.github.com>
2024-06-14 23:51:16 +0800

    Update omni_venus.mk

--- omni_venus.mk
+++ omni_venus.mk
@@ -1,20 +1,3 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
-# Inherit from venus device
-$(call inherit-product, device/xiaomi/venus/device.mk)
-
 PRODUCT_DEVICE := venus
 PRODUCT_NAME := omni_venus
 PRODUCT_BRAND := Xiaomi
